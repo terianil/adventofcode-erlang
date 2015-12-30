e5a4e5f8cec6119e39c62a1bdbe9cf1fe81785eb
Kamil terianil Maciejczek <maciejczek.kamil@gmail.com>
2015-12-30 19:17:47 +0100

    Day2b completed

--- day2b.erl
+++ day2b.erl
@@ -1,12 +1,12 @@
--module(day2a).
+-module(day2b).
 -export([main/1]).
 -mode(compile).
 
-compute_area(X, Y, Z) -> 2*(X*Y) + 2*(Y*Z) + 2*(Z*X).
+compute_volume(X, Y, Z) -> X*Y*Z.
 
-get_minimal_area(X, Y, Z) -> lists:min([(X*Y), (Y*Z), (Z*X)]).
+get_minimal_perimeter(X, Y, Z) -> lists:min([(2*X+2*Y), (2*Y+2*Z), (2*Z+2*X)]).
 
-compute_total_area(X, Y, Z) -> compute_area(X, Y, Z) + get_minimal_area(X, Y, Z).
+compute_total_lenght(X, Y, Z) -> compute_volume(X, Y, Z) + get_minimal_perimeter(X, Y, Z).
 
 load_file(In) -> {ok, File} = file:read_file(In),
                   List = binary_to_list(File),
@@ -16,7 +16,7 @@ load_file(In) -> {ok, File} = file:read_file(In),
                   lists:map(fun(X) -> list_to_tuple(X) end, Boxes).
 
 main(In) -> Boxes = load_file(In),
-            Areas = lists:map(fun({X, Y, Z})-> compute_total_area(X, Y, Z) end, Boxes),
-            TotalArea = lists:sum(Areas),
-            io:write(TotalArea),
+            Lengths = lists:map(fun({X, Y, Z})-> compute_total_lenght(X, Y, Z) end, Boxes),
+            Length = lists:sum(Lengths),
+            io:write(Length),
             io:fwrite("\n").
